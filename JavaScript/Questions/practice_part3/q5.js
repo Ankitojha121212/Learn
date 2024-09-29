2dc86ff588a1f149747fcb44c80087fa3c3bc257
Ankit Ojha <Ankitojha121212@gmail.com>
2024-09-29 12:03:35 +0530

    JavaScript Part 3 completed with assignment Questions

---
+++ JavaScript/Questions/practice_part3/q5.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ *  Qs5. Write a JavaScript program to strip leading and trailing spaces from a string.
+ */
+
+let char = '     asdfhLHjkdKUBJKBn     ';
+
+char = char.trim();
+console.log(char);
---
+++ JavaScript/Questions/practice_part3/q6.js
@@ -0,0 +1,11 @@
+/**
+ *  Qs6. Write a JavaScript program to check if an element exists in an array or not.
+ */
+
+let array = ['mango','gango','tango','fanta','santa','denta','alpha'];
+
+if(array.includes('mansgo')){
+    console.log("present");
+}else{
+    console.log("Absent");
+}
