8443a853873149b9e3ca753f839461fd3ca2ac73
Ankit Ojha <Ankitojha121212@gmail.com>
2024-10-01 12:47:24 +0530

    javaScript Part 8 half completed

---
+++ JavaScript/part8/map.js
@@ -0,0 +1,7 @@
+let arr = [1,2,3,4,5,4,3,2,1,2,3,444,222,342,2,2,0];
+
+let double = arr.map(st =>{
+    console.log(st*2);
+})
+
+// console.log(double);
---
+++ JavaScript/part8/reduce.js
@@ -0,0 +1,5 @@
+let arr = [1,2,3,4];
+
+const finalVal = arr.reduce((res,el) => {return res+el});
+
+console.log(finalVal);
