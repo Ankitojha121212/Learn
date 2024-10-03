e3a77b30d3ca762235c9236a3168680ef42633df
Ankit Ojha <Ankitojha121212@gmail.com>
2024-10-03 10:09:52 +0530

    Call Back Hell

--- JavaScript/part10/callBackHell.js
+++ JavaScript/part10/callBackHell.js
@@ -1,36 +1,39 @@
-
-function saveToDB(data,success,failure){
-    let internetSpeed = Math.floor(Math.random()*10);   // 0 to 10;
+function saveToDB (data,success,failure){
+    let internetSpeed = Math.floor(Math.random()*10)+1;
 
     if(internetSpeed > 4){
         success();
     }else{
         failure();
     }
-
 }
 
+
 saveToDB(
-    "Happy Threads",() =>{
-        console.log("Success 1");
-        saveToDB(
-            "Anjali Boutique",
-            () =>{
-                console.log("Sucess 2 : DATA SAVED");
-                saveToDB(
-                    "Tech Startup",
-                      () =>{
-                        console.log("Sucess 3 : Data Saved");
-                    },() =>{
-                        console.log("Failure 3 : Data was not Saved");
-                    }
-                )
-          },() =>{
-                console.log("Failure 2 : Data Was not saved");
-            }
-        )
-    },
-    () =>{
-        console.log("Failure !!!");
+    "92",()=>{
+        console.log("Success 1 : Data Saved");
+            saveToDB(
+                "93",()=>{
+                    console.log("Success 2 : Data Saved");
+                            saveToDB(
+                                "94",() =>{
+                                    console.log("Success 3 : Data Saved");
+                                            saveToDB(
+                                                "95",()=>{
+                                                    console.log("Sucess 4 : Data Saved");
+                                                },  ()=>{
+                                                    console.log("Failure 4 : Data Not Saved");
+                                                }
+                                            )
+                                },   ()=>{
+                                    console.log("Failure 3 : Data Not Saved");
+                                }
+                            )
+                }, () =>{
+                    console.log("Failure 2 : Data Not Saved");
+                }
+            )
+    }, () =>{
+        console.log("Failure 1 : Data Not Saved");
     }
 )
