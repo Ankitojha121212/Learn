f92e24aadd1c32bcc43d7d424643466e51c8a71e
Ankit Ojha <Ankitojha121212@gmail.com>
2024-10-05 17:05:01 +0530

    Require and module.exports function completed

--- BackEnd/Node/moduleExports/script.js
+++ BackEnd/Node/moduleExports/script.js
@@ -1,1 +1,9 @@
-let someValue = require('./math.js');
+// let someValue = require('./math.js');
+// console.log(someValue);
+
+//////////////////////////////
+////  Way 2
+
+let math = require('./math');
+console.log(math.PI);
+console.log(math.add(34,23));
