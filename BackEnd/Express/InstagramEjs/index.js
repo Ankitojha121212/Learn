f83c85605b41e426f1bbc2edc3f1481a7c37ed2b
Ankit Ojha <Ankitojha121212@gmail.com>
2024-10-06 17:48:47 +0530

    Ejs Instagram Template leraning completed

--- BackEnd/Express/InstagramEjs/index.js
+++ BackEnd/Express/InstagramEjs/index.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
-
+const path = require("path");
 const port = 8080;
 
 
+//app.use(express.static("public"));  // throuh that we can access it with only same directory
+ app.use(express.static(path.join(__dirname,"public/css"))); // now it can accessed from it parent directory as well
+ app.use(express.static(path.join(__dirname,"public/js"))); // now it can accessed from it parent directory as well
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+
 app.get("/ig/:username",(req,res)=>{
-    let {username} = req.params;
-    res.render("instagram.ejs",{username})
+    const {username} = req.params;
+    const instadata = require('./data.json');
+
+    const data =  instadata[username];
+    if(data){
+        res.render("instagram.ejs",{ data });
+    }else{
+        res.render("error.ejs");
+    }
+
 })
 
 app.listen(port,()=>{
---
+++ BackEnd/Express/InstagramEjs/public/js/app.js
@@ -0,0 +1,7 @@
+const btns = document.querySelectorAll("button");
+
+for( bt of btns){
+    bt.addEventListener("click",()=>{
+        console.log("Button was Clicked!!");
+    })
+}
