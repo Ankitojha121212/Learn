ca73d02149764859fc0341faec863e6c780f8964
Ankit Ojha <Ankitojha121212@gmail.com>
2024-10-07 16:19:15 +0530

    Ejs

--- BackEnd/Express/InstagramEjs/public/js/app.js
+++ BackEnd/Express/InstagramEjs/public/js/app.js
@@ -1,6 +1,6 @@
-const btns = document.querySelectorAll("button");
-
+const btns = document.querySelectorAll("button");  
 for( bt of btns){
+
     bt.addEventListener("click",()=>{
         console.log("Button was Clicked!!");
     })
