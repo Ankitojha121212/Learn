6292011156b03fa6a3ede953e0b83782dc73d5c2
Ankit Ojha <Ankitojha121212@gmail.com>
2024-10-07 18:00:57 +0530

    Learn about handling the Post request body data

--- BackEnd/Miscellaneous/Backend/index.js
+++ BackEnd/Miscellaneous/Backend/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+// for post request data ussage
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
 const port = 8080;
 
 app.get("/register" ,(req,res)=>{
@@ -9,6 +12,8 @@ app.get("/register" ,(req,res)=>{
     res.send(`Standard Get Request !! Welcome ${username}`);
 })
 app.post("/register" ,(req,res)=>{
+
+    console.log(req.body);  // without middleware or without pre Express recoginition we can not access the req.body data for that we have to predefine the type of data format and urldata endcoded type
     res.send("Standard Post response");
 })
 
