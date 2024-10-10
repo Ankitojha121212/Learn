6946e44802d75b5048e6355c99a23ce2d69b0805
Ankit Ojha <Ankitojha121212@gmail.com>
2024-10-10 16:56:00 +0530

    Update route and edit route was Created with the edit ejs page but it is not working that way so i am leaving the update route at that time and after some i will reconsider this :

--- BackEnd/Mongo3/index.js
+++ BackEnd/Mongo3/index.js
@@ -4,6 +4,7 @@ const port = 8080;
 const mongoose = require('mongoose');
 const path = require('path');
 const Chat = require("./model/chat");
+const methodOverride = require("method-override");
 
 
 app.set("views",path.join(__dirname,"views"));
@@ -11,6 +12,7 @@ app.set("view engine","ejs");
 
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
 
 async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
@@ -66,6 +68,25 @@ app.post("/chats",(req,res)=>{
    });
 
    res.redirect("/chats");
+})
+
+// edit route
+app.get("/chats/:id/edit",async(req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs",{chat});
+
+})
+
+app.put("/chats/:id", (req,res)=>{
+    let {id} = req.params;
+    let {message : newMessage} = req.body;
+    let updatedChat = Chat.findByIdAndUpdate(id, { message : newMessage}, { new: true });
+    res.redirect("/chats");
+    console.log(updatedChat);
+
+
+    
 })
 
 
