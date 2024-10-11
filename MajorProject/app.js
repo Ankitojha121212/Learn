ae7d1624e9a49a65dbf06b6420f28b505663415c
Ankit Ojha <Ankitojha121212@gmail.com>
2024-10-11 15:39:19 +0530

    new Route working for the adding listing of the Wonderlust

--- MajorProject/app.js
+++ MajorProject/app.js
@@ -8,6 +8,8 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
 
+app.use(express.urlencoded({extended : true}));
+
 async function main(){
     await mongoose.connect(mongoUrl);
 }
@@ -26,6 +28,20 @@ app.get("/listings", async (req,res)=>{
    const allListings = await Listing.find({});
    res.render("listings/index.ejs",{allListings});
 })
+// new route for creating the new listing
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+})
+
+// show route
+app.get("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+
+})
+
+
 // // test listing
 // app.get("/testListing", async (req,res)=>{
 //     const sampleListing = new Listing({
