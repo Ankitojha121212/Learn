6b1a389ec9cc4c7a4d2841f5a9b254e445734035
Ankit Ojha <Ankitojha121212@gmail.com>
2024-10-11 13:20:44 +0530

    projects main page routing and listing of title with specific id route is done in links only not in the form of cards

--- MajorProject/app.js
+++ MajorProject/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const Listing = require("./models/listing");
-
 const mongoose = require("mongoose");
-
 const mongoUrl = "mongodb://127.0.0.1/wonderlust";
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"views"));
 
 async function main(){
     await mongoose.connect(mongoUrl);
@@ -19,19 +21,24 @@ app.get("/",(req,res)=>{
     res.send("On root page");
 })
 
-// listing
-app.get("/testListing", async (req,res)=>{
-    const sampleListing = new Listing({
-        title : "Noida me villa",
-        description : "Ye h Noida ka Villa jo kr deta h Sbko Gilla, mat krna iska paani pila pila < Samjhe rangila >",
-        price : 50000,
-        location : "Noida",
-        country : "India"
-    });
-  await  sampleListing.save();
-    console.log("sample data saved");
-    res.send("TestListing working");
+
+app.get("/listings", async (req,res)=>{
+   const allListings = await Listing.find({});
+   res.render("listings/index.ejs",{allListings});
 })
+// // test listing
+// app.get("/testListing", async (req,res)=>{
+//     const sampleListing = new Listing({
+//         title : "Noida me villa",
+//         description : "Ye h Noida ka Villa jo kr deta h Sbko Gilla, mat krna iska paani pila pila < Samjhe rangila >",
+//         price : 50000,
+//         location : "Noida",
+//         country : "India"
+//     });
+//   await  sampleListing.save();
+//     console.log("sample data saved");
+//     res.send("TestListing working");
+// })
 
 app.listen(8080,()=>{
     console.log("Server started");
