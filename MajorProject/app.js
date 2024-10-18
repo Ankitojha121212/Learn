6607d44bf5940618bf6c2e9972d4246c45cbe374
Ankit Ojha <Ankitojha121212@gmail.com>
2024-10-18 18:43:52 +0530

    My review section is working now

--- MajorProject/app.js
+++ MajorProject/app.js
@@ -69,7 +69,7 @@ app.use((req,res,next)=>{
 
 // Connecting the routes files with Main file
 app.use("/listings",listings);
-app.use("/listings/:id/review",reviews);
+app.use("/listings/:id/reviews",reviews);
 
 
 
