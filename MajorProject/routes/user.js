4cd232932cc94e3fbef5610f890479c1c472e98d
Ankit Ojha <Ankitojha121212@gmail.com>
2024-10-19 18:15:12 +0530

    Authentication is completed

--- MajorProject/routes/user.js
+++ MajorProject/routes/user.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const User = require('../models/user.js');
 const wrapAsync = require('../utils/wrapAsync');
 const passport = require('passport');
+const { saveRedirectUrl } = require('../middleware.js');
 
 
 
-
+////////// SignUp process
 router.get("/signup",(req,res)=>{
     res.render('users/signup.ejs');
 })
@@ -20,10 +21,16 @@ router.post("/signup", wrapAsync( async (req,res)=>{
         const newUser = new User({email,username});
         
         const registeredUser = await User.register(newUser,password);
+        req.login(registeredUser,(err)=>{
+            if(err){
+                return next(err);
+            }
+            req.flash("success","Registered Successfully !!!");
+            // console.log(registeredUser);
+            res.redirect("/listings");
+
+        })
         
-        req.flash("success","Registered Successfully !!!");
-        console.log(registeredUser);
-        res.redirect("/listings");
     }catch(e){
         req.flash("error",e);
         res.redirect("/listings");
@@ -39,11 +46,13 @@ router.get("/login",(req,res)=>{
 });
 
 
-router.post("/login", passport.authenticate('local',{failureRedirect:"/login",failureFlash:true}) ,async (req,res) =>{
-
+router.post("/login", saveRedirectUrl,
+     passport.authenticate('local',{failureRedirect:"/login",failureFlash:true}) ,
+     async (req,res) =>{
     let {username} = req.body;
     req.flash("success",`Happy to see you  ${username} Again !!`);
-    res.redirect("/listings");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
 
 })
 
