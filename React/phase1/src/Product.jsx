cbfa1a1ee8a3196770caf19d5f918a63ce6ac9c7
Ankit Ojha <Ankitojha121212@gmail.com>
2024-10-27 13:54:18 +0530

    learning about the array as props

--- React/phase1/src/Product.jsx
+++ React/phase1/src/Product.jsx
@@ -1,10 +1,12 @@
 import './Product.css';
-const Product = ({title,description}) => {
+const Product = ({title,description,features}) => {
+
   return (
     <>
     <div className="product" >
         <h3>Title {title} </h3>
         <p>Description :{description} </p>
+        <p>Features :{features} </p>
     </div>
     </>
   )
--- React/phase1/src/ProductTab.jsx
+++ React/phase1/src/ProductTab.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Product from './Product'
 const ProductTab = () => {
+  let features = ['fast charging',"less space",'lowest price'];
   return (
     <div>
-      <Product title="Laptop" description="Mera h bhai laptop" />
-      <Product title="Charger" description="Nhi Dunga" />
+      <Product title="Laptop" description="Mera h bhai laptop" features={features} />
+      <Product title="Charger" description="Nhi Dunga" features={features} />
       <Product title="Phone" description="Mast h" />
-      <Product title="Pataka" description="Pta lenge" />
+      <Product title="Pataka" description="Pta lenge" features={features} />
     </div>
   )
 }
