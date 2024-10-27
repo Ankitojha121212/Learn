ec02a95baf861f865d1e2c1fdc8403c9ae6445c7
Ankit Ojha <Ankitojha121212@gmail.com>
2024-10-27 18:45:28 +0530

    Learn about mapping the features in html tag

--- React/phase1/src/Product.jsx
+++ React/phase1/src/Product.jsx
@@ -6,7 +6,9 @@ const Product = ({title,description,features}) => {
     <div className="product" >
         <h3>Title {title} </h3>
         <p>Description :{description} </p>
-        <p>Features :{features} </p>
+        <p>Features :{features.map((feature)=>(
+          <li>{feature}</li>
+        ))} </p>
     </div>
     </>
   )
--- React/phase1/src/ProductTab.jsx
+++ React/phase1/src/ProductTab.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+// import React from 'react'
 import Product from './Product'
+// const ProductTab = () => {
+//   let features = ['fast charging',"less space",'lowest price'];
+//   return (
+//     <div>
+//       <Product title="Laptop" description="Mera h bhai laptop" features={features} />
+//       <Product title="Charger" description="Nhi Dunga" features={features} />
+//       <Product title="Phone" description="Mast h"features={features[0]}/>
+//       <Product title="Pataka" description="Pta lenge" features={features} />
+//     </div>
+//   )
+// }
+
+// export default ProductTab
+
+
+import React from 'react'
+
 const ProductTab = () => {
-  let features = ['fast charging',"less space",'lowest price'];
+
+  // const features = [<li>"fast charging"</li>,<li>"slow charging"</li>];
+  const option =  ["fast charging","slow charging","large storage","521 Gb ram"];
+
   return (
     <div>
-      <Product title="Laptop" description="Mera h bhai laptop" features={features} />
-      <Product title="Charger" description="Nhi Dunga" features={features} />
-      <Product title="Phone" description="Mast h" />
-      <Product title="Pataka" description="Pta lenge" features={features} />
+       <Product title="Laptop" description="Mera h bhai laptop" features={option} />
+      <Product title="Charger" description="Nhi Dunga" features={option} />
     </div>
   )
 }
 
 export default ProductTab
+
